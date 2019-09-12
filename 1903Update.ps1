b55aa3460cad88334c2607f894d09fd810cd6ea2
Jared <jared@dkbinnovative.net>
2019-09-12 14:45:12 -0500

    Update 1903Update.ps1

--- 1903Update.ps1
+++ 1903Update.ps1
@@ -35,7 +35,7 @@ Write-Output "Installation start time triggered by user: $dateTime" | Out-File $
 #region checkDisk
 $spaceAvailable = [math]::round((Get-PSDrive C | Select -ExpandProperty Free) / 1GB,0)
 If ($spaceAvailable -lt 15) {
-  Write-Output "You only have a total of $spaceAvailable GBs available, this upgrade needs 10GBs or more to complete successfully" | Out-File $logFile -Append
+  Write-Output "You only have a total of $spaceAvailable GBs available, this upgrade needs 10GBs or more to complete successfully: $dateTime" | Out-File $logFile -Append
   Break
 }
 #region checkDisk
@@ -47,20 +47,20 @@ $rbCheck2 = Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpd
 $rbCheck3 = Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore
 
 If ($rbCheck1 -ne $Null -or $rbCheck2 -ne $Null -or $rbCheck3 -ne $Null){
-    Write-Output "This system is pending a reboot, unable to proceed. Please restart your computer and try again." | Out-File $logFile -Append
+    Write-Output "This system is pending a reboot, unable to proceed. Please restart your computer and try again.: $dateTime" | Out-File $logFile -Append
     Break
 } Else {
-    Write-Output "Automation has verified there is no reboot pending." | Out-File $logFile -Append
+    Write-Output "Automation has verified there is no reboot pending - $dateTime" | Out-File $logFile -Append
 }
 
 If ([System.Environment]::OSVersion.Version.Major -ne 10) {
-  Write-Output "Your version of Windows does not support the 1903 upgrade. Exiting script." | Out-File $logFile -Append
+  Write-Output "Your version of Windows does not support the 1903 upgrade. Exiting script. $dateTime" | Out-File $logFile -Append
   Break
 }
 
 $osBuild = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ReleaseId).ReleaseId
 If ($osBuild -ge 1903) {
-  Write-Output "Your machine already has this patch installed! Exiting script." | Out-File $logFile -Append
+  Write-Output "Your machine already has this patch installed! Exiting script. $dateTime" | Out-File $logFile -Append
   Break
 }
 
@@ -93,7 +93,7 @@ Try {
     New-Item -ItemType Directory -Path $1903Dir | Out-Null
   }
 } Catch {
-  Write-Error "Failed to create the following folder: $1903Dir" | Out-File $logFile -Append
+  Write-Error "Failed to create the following folder: $1903Dir - $dateTime" | Out-File $logFile -Append
   Return
 }
 
@@ -105,21 +105,21 @@ If ($checkZip) {
     $checkFile = Test-Path $1903Zip -PathType Leaf
     If (!$checkFile) {
       $status = 'Download'
-      Write-Output "The existing installation files for the 1903 update were incomplete or corrupt. Deleted existing files and started a new download." | Out-File $logFile -Append
+      Write-Output "The existing installation files for the 1903 update were incomplete or corrupt. Deleted existing files and started a new download. - $dateTime" | Out-File $logFile -Append
     }
     Else {
-      Write-Output "Failed to delete the installation files for 1903. Exiting script." | Out-File $logFile -Append
+      Write-Output "Failed to delete the installation files for 1903. Exiting script. $dateTime" | Out-File $logFile -Append
       Break
     }
   }
   Else {
-    Write-Output "Verified the installation package downloaded successfully!" | Out-File $logFile -Append
+    Write-Output "Verified the installation package downloaded successfully! - $dateTime" | Out-File $logFile -Append
     $status = 'Unzip'
   }
 }
 Else {
   $status = 'Download'
-  Write-Output "The required files to install the 1903 update are not present, downloading required files now. Keep in mind this update is VERY large, and may take 30min+ to download (depending on your connection speed)." | Out-File $logFile -Append
+  Write-Output "The required files to install the 1903 update are not present, downloading required files now. Keep in mind this update is VERY large, and may take 30min+ to download (depending on your connection speed). - $dateTime" | Out-File $logFile -Append
 }
 #endregion fileChecks
 
@@ -130,13 +130,13 @@ If ($status -eq 'Download') {
     (New-Object System.Net.WebClient).DownloadFile($automate1903URL,$1903Zip)
     ## Again check the downloaded file size vs the server file size
     If ($servFile -gt (Get-Item $1903Zip).Length) {
-      Write-Error "The downloaded size of $1903Zip does not match the server version, unable to install the update." | Out-File $logFile -Append
+      Write-Error "The downloaded size of $1903Zip does not match the server version, unable to install the update. - $dateTime" | Out-File $logFile -Append
     } Else {
-      Write-Output 'Successfully downloaded the 1903 update!' | Out-File $logFile -Append
+      Write-Output "Successfully downloaded the 1903 update! - $dateTime" | Out-File $logFile -Append
       $status = 'Unzip'
     }
   } Catch {
-    Write-Error 'Encountered a problem when trying to download the Windows 10 1903 ISO' | Out-File $logFile -Append
+    Write-Error "Encountered a problem when trying to download the Windows 10 1903 ISO - $dateTime" | Out-File $logFile -Append
   }
 }
 
@@ -146,15 +146,15 @@ Try {
     If (!$7zipCheck) {
       (New-Object System.Net.WebClient).DownloadFile($automate7zipURL,$7zip)
     }
-    Write-Output 'Unpacking 1903 installation files...this will take awhile.' | Out-File $logFile -Append
+    Write-Output "Unpacking 1903 installation files...this will take awhile. - $dateTime" | Out-File $logFile -Append
     &$7zip x $1903Zip -o"$1903Dir" -y | Out-Null
-    Write-Output 'Unpacking complete! Beginning 1903 upgrade installation.' | Out-File $logFile -Append
+    Write-Output "Unpacking complete! Beginning 1903 upgrade installation. - $dateTime" | Out-File $logFile -Append
     $status = 'Install'
   }
 
   ##Install
   If ($status -eq 'Install') {
-    Write-Output 'The 1903 upgrade installation has now been started silently in the background. No action from you is required, but please note a reboot will be reqired during the installation prcoess. It is highly recommended you save all of your open files!' | Out-File $logFile -Append
+    Write-Output "The 1903 upgrade installation has now been started silently in the background. No action from you is required, but please note a reboot will be reqired during the installation prcoess. It is highly recommended you save all of your open files! - $dateTime" | Out-File $logFile -Append
     $localFolder= (Get-Location).path
     $process = Start-Process -FilePath "$1903Dir\Pro$osVer.1903\setup.exe" -ArgumentList "/auto upgrade /quiet" -PassThru
     For($i = 0; $i -le 100; $i = ($i + 1) % 100) {
@@ -162,9 +162,9 @@ Try {
       Start-Sleep -Milliseconds 100
       If ($process.HasExited) {
         Write-Progress -Activity "Installer" -Completed
-        Write-Output "Installation has completed, deleting setup files" | Out-File $logFile -Append
+        Write-Output "Installation has completed, deleting setup files - $dateTime" | Out-File $logFile -Append
         Remove-Tree -Path $1903Dir
-        Write-Output "1903 upgrade process complete!"
+        Write-Output "1903 upgrade process complete! - $dateTime"
         Break
       }
     }
@@ -172,6 +172,6 @@ Try {
       Write-Error "Could not find a known status of the var Status. Output: $status" | Out-File $logFile -Append
   }
 } Catch {
-  Write-Error 'Setup ran into an issue while attempting to install the 1903 upgrade.'
+  Write-Error "Setup ran into an issue while attempting to install the 1903 upgrade. - $dateTime"
 }
 #endregion download/install
